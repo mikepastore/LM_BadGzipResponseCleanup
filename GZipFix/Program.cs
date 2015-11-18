337c48e89cc94d14c659706864df3c56c6cf09da
mikepastore <mpastore@accela.com>
2015-11-18 17:05:32 -0500

    Initial commit

---
+++ GZipFix/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+
+namespace GZipFix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(var file in Directory.GetFiles(args[0],"*.rtf"))
+            {
+                Repair(file);
+            }            
+        }
+
+        private static void Repair(string file)
+        {
+            string dest = Path.GetDirectoryName(file) + @"\out\" + Path.GetFileName(file);
+            Directory.CreateDirectory(Path.GetDirectoryName(file) + @"\out\");
+            File.Copy(file, dest, true);
+
+            int tries = 0;
+            while(!IsValidRTF(dest) && ++tries < 100)            
+                DecompressFile(dest);
+        }
+
+        private static bool IsValidRTF(string file)
+        {
+            var bytes = File.ReadAllBytes(file);
+            return bytes.All(b=> b < 128);
+        }
+
+        private static void DecompressFile(string file)
+        {
+            byte[] buffer;
+            using (var fileStream = File.OpenRead(file))
+            {
+                using (var compressedStream = new GZipStream(fileStream, CompressionMode.Decompress))
+                {                   
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        try
+                        {
+                            compressedStream.CopyTo(memoryStream);
+                            memoryStream.Seek(0, SeekOrigin.Begin);
+                            buffer = new byte[memoryStream.Length];
+                            memoryStream.Read(buffer, 0, (int)memoryStream.Length);
+                        }
+                        catch (Exception e)
+                        {
+                            throw;
+                        }
+                    }
+                }
+            }
+
+            File.Create(file).Close();
+            File.WriteAllBytes(file, buffer);
+        }
+
+        
+
+    }
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+Tool to clean up bad responses from LMAM-114. Simply decompresses files in a folder repeatedly until they become valid text
